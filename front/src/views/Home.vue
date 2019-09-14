aa797122ba54e1fd224a64420f6a732e7b941fa4
Pliavi <pliavi.rpgm@gmail.com>
2019-09-14 17:18:32 -0300

    Some fixed in components and remove unused ones

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="home">
-    <Ola />
+    <Header />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Ola from "@/components/HelloWorld.vue";
+import Header from "@/components/Header.vue";
 
 export default {
   name: "home",
   components: {
-    Ola
+    Header
   }
 };
 </script>
