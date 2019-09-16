713a599716a81859721e01f9c8bfe69fcb652c25
guigranzeol <guilherme.granzeol@gmail.com>
2019-09-16 17:29:28 -0300

    Começando Criação Tabs

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,17 +1,21 @@
 <template>
   <div class="home">
     <Header />
+    <Tab title="Times" sub-title="Veja onde você se encaixa" />
+    <Tab title="Participantes" sub-title="Encontra o melhor pra sua equipe" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Header from "@/components/Header.vue";
+import Tab from "@/components/Tab.vue";
 
 export default {
   name: "home",
   components: {
-    Header
+    Header,
+    Tab
   }
 };
 </script>
