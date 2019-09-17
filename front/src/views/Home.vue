99a279f8733202451498a19df8b469bb47936dd9
Pliavi <pliavi.rpgm@gmail.com>
2019-09-17 16:47:46 -0300

    Remove comments

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -5,13 +5,10 @@
 </template>
 
 <script>
-// @ is an alias to /src
 import Header from "@/components/Header.vue";
 
 export default {
   name: "home",
-  components: {
-    Header
-  }
+  components: { Header }
 };
 </script>
